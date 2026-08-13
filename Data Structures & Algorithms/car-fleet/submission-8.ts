63cefc539861f20b6b4abac469ce77d018f17dc3
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-13 09:54:34 -0600

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target: number, position: number[], speed: number[]): number {
+        const result = [];
+
+        const combinedData = position.map((val, index) => {
+            return [val, speed[index]];
+        });
+
+        combinedData.sort((a, b) => b[0] - a[0]);
+
+        const time = combinedData.map((val) => {
+            return (target - val[0]) / val[1];
+        });
+
+        console.log(time);
+
+        time.forEach((val) => {
+            if (result.length === 0) {
+                result.push(val);
+            } else if (result.at(-1) < val) {
+                result.push(val);
+            }
+        });
+
+        return result.length;
+    }
+}
