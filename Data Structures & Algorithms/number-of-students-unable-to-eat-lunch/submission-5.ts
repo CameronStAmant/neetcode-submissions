1f127f3a47ffb8e3144ee2af56387131bc539079
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-14 09:06:32 -0600

    Add: number-of-students-unable-to-eat-lunch - submission-5

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-5.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} students
+     * @param {number[]} sandwiches
+     * @return {number}
+     */
+    countStudents(students: number[], sandwiches: number[]): number {
+        let i = 0;
+
+        while (students.length !== 0 && i <= students.length - 1) {
+        const currentStudent = students.shift();
+            if (currentStudent === sandwiches[0]) {
+                sandwiches.shift();
+                i = 0;
+            } else {
+                students.push(currentStudent);
+                i++;
+            }
+        }
+
+        return students.length;
+    }
+}
