c42b79bf0121a21c13f529346ef62c4f0af7d719
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-17 10:03:33 -0600

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.ts
@@ -0,0 +1,40 @@
+class MinStack {
+    private stack: number[];
+    private minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+        this.minStack.push(val > this.minStack.at(-1) ? this.minStack.at(-1) : val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack.at(-1);
+    }
+}
