accab356c9924b4892572ebbf30722ec75b02fe7
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-26 12:15:00 -0600

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.ts
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        
+        if (s.length !== t.length) {
+            return false;
+        }
+        
+        const sMap = new Map();
+        const tMap = new Map();
+        
+        for (const char of s) {
+            if (sMap.has(char)) {
+                sMap.set(char, sMap.get(char) + 1);
+            } else {
+                sMap.set(char, 1);
+            }
+        }
+
+        for (const char of t) {
+            if (tMap.has(char)) {
+                tMap.set(char, tMap.get(char) + 1);
+            } else {
+                tMap.set(char, 1);
+            }
+        }
+
+        let anagram = true;
+
+        sMap.forEach((value, key) => {
+            if (tMap.has(key) && tMap.get(key) === value) {
+            } else {
+                anagram = false;
+            }
+        });
+
+        return anagram;
+    }
+}
