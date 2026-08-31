3357dde9279bf8973b38a1399e7c8347d7cb9d61
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-31 14:59:01 -0600

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let lPointer = 0;
+        let rPointer = s.length - 1;
+
+        while (lPointer < rPointer) {
+            while (!isAlphaNumeric(s[lPointer]) && lPointer < rPointer) {
+                lPointer++;
+            }
+
+            while (!isAlphaNumeric(s[rPointer]) && lPointer < rPointer) {
+                rPointer--;
+            }
+            
+            if (s[lPointer].toLowerCase() !== s[rPointer].toLowerCase()) return false;
+
+            lPointer++;
+            rPointer--;
+        }
+
+        return true;
+    }
+}
+
+function isAlphaNumeric (char: string) {
+    if ((char >= 'a' && char <= "z") || (char >= 'A' && char <= "Z") || (char >= '0' && char <= '9')) return true;
+    return false;
+}
