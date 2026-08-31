f0814142f1fdb82af1e340f49a3d9dbc86afd4f8
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-31 15:08:40 -0600

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let leftPointer = 0;
+        let rightPointer = numbers.length - 1;
+
+        while (leftPointer < rightPointer) {
+            if (numbers[leftPointer] + numbers[rightPointer] < target) {
+                leftPointer++;
+            } else if (numbers[leftPointer] + numbers[rightPointer] > target) {
+                rightPointer--;
+            } else {
+                return [leftPointer + 1, rightPointer + 1];
+            }
+        }
+
+    }
+}
