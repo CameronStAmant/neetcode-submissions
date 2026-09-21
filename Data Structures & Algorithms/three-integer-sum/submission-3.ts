6c1d57d773fc0523d518c2bec69a19df24f40444
Cameron St. Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-09-21 16:58:33 -0600

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        let res = [];
+        nums.sort((a, b) => a - b);
+        nums.map((outerNum, i) => {
+            if (outerNum !== nums[i - 1]) {
+                let leftPointer = i + 1;
+                let rightPointer = nums.length - 1;
+
+                while (leftPointer < rightPointer) {
+                    if (
+                        outerNum + nums[leftPointer] + nums[rightPointer] < 0 &&
+                        leftPointer < rightPointer
+                    ) {
+                        leftPointer++;
+                    }
+
+                    if (
+                        outerNum + nums[leftPointer] + nums[rightPointer] > 0 &&
+                        leftPointer < rightPointer
+                    ) {
+                        rightPointer--;
+                    }
+                    if (
+                        outerNum + nums[leftPointer] + nums[rightPointer] === 0 &&
+                        leftPointer < rightPointer
+                    ) {
+                        res.push([outerNum, nums[leftPointer], nums[rightPointer]]);
+                        while (outerNum + nums[leftPointer] + nums[rightPointer] === 0) {
+                            leftPointer++;
+                        }
+                    }
+                }
+            }
+        });
+        return res;
+    }
+}
